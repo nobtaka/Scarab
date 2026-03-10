94e5272ff8a8597f31d6346cf66cd4852042b503
Nob Nukui <nob@superluckyboy.com>
2026-03-10 13:04:20 +0900

    Initial Commit

---
+++ Scarab/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Scarab
+//
+//  Created by Nob Nukui on 2026/03/10.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ ScarabTests/ScarabTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScarabTests.swift
+//  ScarabTests
+//
+//  Created by Nob Nukui on 2026/03/10.
+//
+
+import Testing
+@testable import Scarab
+
+struct ScarabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
